d977b0bfb42b27b2bd6242cfb0a13a601336219e
Stanislav Kryzhanovskiy <stanislavkryzhanovsky@gmail.com>
2019-11-19 22:43:46 +0300

    install .env

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,47 @@
 <template>
-  <div id="app" class="app">
-    <telegramme
-      BotApi='786698677:AAH0nMPQtXQDF_ZEw2w5tA68kno91JQJ_TI'
-      chatID="-1001303367026"
-      title='Telegram me!'
-      sendButton='Send'
-      repeatButton="Send again"
-      sendFromPlaceholder='Enter your name...'
-      subjectPlaceholder='Enter your subject...'
-      messagePlaceholder='Create your message...'
-      successNotification="Great! I will see your message ;-)"
-    />
-  </div>
+	<div id="app" class="app">
+		<telegramme
+			:BotApi="BotApi"
+			:chatID="chatID"
+			:title="title"
+			:sendButton="sendButton"
+			:repeatButton="repeatButton"
+			:sendFromPlaceholder="sendFromPlaceholder"
+			:subjectPlaceholder="subjectPlaceholder"
+			:messagePlaceholder="messagePlaceholder"
+			:successNotification="successNotification"
+		/>
+	</div>
 </template>
 
 <script>
-import telegramme from './components/telegram-me.vue'
+import telegramme from './components/telegram-me.vue';
 
 export default {
-  name: 'app',
-  components: {
-    telegramme
-  }
-}
+	name: 'app',
+	components: {
+		telegramme
+	},
+
+	data() {
+		return {
+			BotApi: process.env.VUE_APP_BOTAPI,
+			chatID: process.env.VUE_APP_CHATID,
+			title: process.env.VUE_APP_FORMTITLE,
+			sendButton: process.env.VUE_APP_SENDBUTTON,
+			repeatButton: process.env.VUE_APP_REPEATBUTTON,
+			sendFromPlaceholder: process.env.VUE_APP_SENDFORMPLACEHOLDER,
+			subjectPlaceholder: process.env.VUE_APP_SUBJECTPLACEHOLDER,
+			messagePlaceholder: process.env.VUE_APP_MESSAGEPLACEHOLDER,
+			successNotification: process.env.VUE_APP_SUCCESSNITIFICATION
+		};
+	}
+};
 </script>
 <style scoped>
-  .app {
-    display: flex;
-    justify-content: center;
-    padding: 10%;
-  }
+.app {
+	display: flex;
+	justify-content: center;
+	padding: 10%;
+}
 </style>
